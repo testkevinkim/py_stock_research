f534b01cd92df4e17da6a6162b22a1fdf869d446
keun_young_kim_thd <keun_young_kim@homedepot.com>
2021-10-26 18:09:09 -0400

    a

--- research/us/data_collect/config.py
+++ research/us/data_collect/config.py
@@ -15,8 +15,8 @@ tz_name = "US/Eastern"
 log_path = os.path.join(root_path, "{}us_bid_ask_collect.log".format("test_" if override else ""))
 email_path = os.path.join(root_path, "gmail.json")
 
-first_capture_time = "17:29:00" # 16:05:00
-second_capture_time = "17:59:00" # 17:00:00
+first_capture_time = "16:05:00" # 16:05:00
+second_capture_time = "17:00:00" # 17:00:00
 
 yaml_token_path = os.path.join(root_path, "token.yaml")
 access_token_path = os.path.join(root_path, "access_token.yml")
